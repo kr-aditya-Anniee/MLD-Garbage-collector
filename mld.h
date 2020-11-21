62d47ca1f9a30dd9e16bf496b642ac7c3abda5a2
Kumar Aditya <71898237+kr-aditya-Anniee@users.noreply.github.com>
2020-11-21 17:51:05 +0530

    Update mld.h

--- mld.h
+++ mld.h
@@ -1,35 +1,3 @@
-/*
- * =====================================================================================
- *
- *       Filename:  mld.h
- *
- *    Description:  This file defines the data structures used for MLD tool
- *
- *        Version:  1.0
- *        Created:  Thursday 28 February 2019 05:14:18  IST
- *       Revision:  1.0
- *       Compiler:  gcc
- *
- *         Author:  Er. Abhishek Sagar, Networking Developer (AS), sachinites@gmail.com
- *        Company:  Brocade Communications(Jul 2012- Mar 2016), Current : Juniper Networks(Apr 2017 - Present)
- *        
- *        This file is part of the MLD distribution (https://github.com/sachinites).
- *        Copyright (c) 2017 Abhishek Sagar.
- *        This program is free software: you can redistribute it and/or modify
- *        it under the terms of the GNU General Public License as published by  
- *        the Free Software Foundation, version 3.
- *
- *        This program is distributed in the hope that it will be useful, but 
- *        WITHOUT ANY WARRANTY; without even the implied warranty of 
- *        MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU 
- *        General Public License for more details.
- *
- *        You should have received a copy of the GNU General Public License 
- *        along with this program. If not, see <http://www.gnu.org/licenses/>.
- *
- * =====================================================================================
- */
-
 #ifndef __MLD__
 #include <assert.h>
 #include <string.h>
