61faf018502486625090127333e21cd06b8a6f32
Kumar Aditya <71898237+kr-aditya-Anniee@users.noreply.github.com>
2020-11-21 17:50:01 +0530

    Update mld.c

--- mld.c
+++ mld.c
@@ -1,35 +1,3 @@
-/*
- * =====================================================================================
- *
- *       Filename:  mld.c
- *
- *    Description:  This file implements the functions and routines for mld library
- *
- *        Version:  1.0
- *        Created:  Thursday 28 February 2019 06:17:28  IST
- *       Revision:  1.0
- *       Compiler:  gcc
- *
- *         Author:  Er. Abhishek Sagar, Networking Developer (AS), sachinites@gmail.com
- *        Company:  Brocade Communications(Jul 2012- Mar 2016), Current : Juniper Networks(Apr 2017 - Present)
- *        
- *        This file is part of the MLD distribution (https://github.com/sachinites).
- *        Copyright (c) 2017 Abhishek Sagar.
- *        This program is free software: you can redistribute it and/or modify
- *        it under the terms of the GNU General Public License as published by  
- *        the Free Software Foundation, version 3.
- *
- *        This program is distributed in the hope that it will be useful, but 
- *        WITHOUT ANY WARRANTY; without even the implied warranty of 
- *        MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU 
- *        General Public License for more details.
- *
- *        You should have received a copy of the GNU General Public License 
- *        along with this program. If not, see <http://www.gnu.org/licenses/>.
- *
- * =====================================================================================
- */
-
 #include <stdio.h>
 #include <stdlib.h>
 #include "mld.h"
